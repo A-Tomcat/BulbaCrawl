30671623923ba7378762aafad6e1dbf69aa2d3bf
A-Tomcat <tom@ollfisch.de>
2026-07-14 16:07:54 +0200

    Working on TCG implementation

--- fromHTML.go
+++ fromHTML.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+func (cfg *Config) getNamedURLsFromHTML(htmlBody string) ([]string, error) {
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
 	if err != nil {
 		return nil, err
@@ -22,12 +25,62 @@ func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
 				crawlErr = err
 				return
 			}
-			resolvedURL := baseURL.ResolveReference(parsedHref)
-			links = append(links, resolvedURL.String())
+			if strings.Contains(parsedHref.Path, cfg.SearchName) {
+				resolvedURL := cfg.BaseURL.ResolveReference(parsedHref)
+				links = append(links, resolvedURL.String())
+			}
 		}
 	})
 	if crawlErr != nil {
 		return nil, crawlErr
 	}
 	return links, nil
+}
+
+func (cfg *Config) getHTML() (string, error) {
+	name_parts := strings.Split(cfg.SearchName, " ")
+	searchname_url := strings.Join(name_parts, "_")
+	SearchPath, err := url.Parse("wiki" + searchname_url + "_(" + cfg.Category + ")")
+	if err != nil {
+		return "", err
+	}
+	SearchURL := cfg.BaseURL.ResolveReference(SearchPath)
+	req, err := http.NewRequest("GET", SearchURL.String(), nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("User-Agent", "BulbaCrawler/1.0")
+	client := http.DefaultClient
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode >= 400 {
+		return "", fmt.Errorf("HTTP error, Status Code: %d", res.StatusCode)
+	}
+	content_type := res.Header.Get("Content-Type")
+	if !strings.Contains(content_type, "text/html") {
+		return "", fmt.Errorf("unexpected content type: %s", content_type)
+	}
+
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	htmlString := string(bytes)
+	return htmlString, nil
+}
+
+func (cfg *Config) CaseTCG() error {
+	html, err := cfg.getHTML()
+	if err != nil {
+		return err
+	}
+	urls, err := cfg.getNamedURLsFromHTML(html)
+	if err != nil {
+		return err
+	}
+	cfg.result.TCG.NumberOfCards = len(urls)
+	return nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+type Config struct {
+	BaseURL    *url.URL
+	SearchName string
+	Category   string
+	mu         *sync.Mutex
+	wg         *sync.WaitGroup
+	result     *Result
+}
+
+type Result struct {
+	Pokemon struct {
+		Name      string `json:"name"`
+		BaseStats struct {
+			Hp     int `json:"hp"`
+			Atk    int `json:"atk"`
+			Def    int `json:"def"`
+			Sp_Att int `json:"sp_att"`
+			Sp_Def int `json:"sp_def"`
+			Speed  int `json:"spd"`
+			Total  int `json:"total"`
+		} `json:"base_stats"`
+		Prev_Evolution string   `json:"prev_evolution,omitempty"`
+		Next_Evolution []string `json:"next_evoltion,omitempty"`
+	} `json:"pokemon,omitempty"`
+	TCG struct {
+		Name          string `json:"name"`
+		NumberOfCards int    `json:"cards_released"`
+	} `json:"tcg,omitempty"`
+	Move struct {
+		Name       string `json:"name"`
+		PP         int    `json:"pp"`
+		Power      int    `json:"power"`
+		Accuracy   int    `json:"accuracy"`
+		Priority   int    `json:"priority"`
+		LearnedLVL int    `json:"learned_lvl"`
+		LearnedTM  int    `json:"learner_tm"`
+		LearnedEgg int    `json:"learned_egg"`
+	} `json:"move,omitempty"`
+}
 
 func main() {
 	fmt.Println("Hello Moron.")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		log.Fatal(`Usage: ./pokecrawl <tcg/pokemon/move> "<name>"`)
+	}
+	baseURL := os.Getenv("BASEURL")
+	base_url, err := url.Parse(baseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	category := strings.ToLower(args[0])
+	switch category {
+	case "tcg":
+		category = "TCG"
+	case "move":
+		category = "move"
+	case "pokemon":
+		category = "pokémon"
+	default:
+		log.Fatal("Category not unique, Options: <move>, <tcg>, <pokemon>")
+	}
+	searchname := cases.Title(language.English).String(args[1])
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	result := Result{}
+	cfg := Config{
+		BaseURL:    base_url,
+		SearchName: searchname,
+		Category:   category,
+		mu:         &mutex,
+		wg:         &wg,
+		result:     &result,
+	}
+	fmt.Println(cfg)
 }
